1f985b97e04f8c9101e0097da8e052f5fac239e8
kkorvel <koit.korvel@gmail.com>
2016-10-29 17:36:11 +0300

    uued

---
+++ src/Harjutused.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Koit on 29.10.2016.
+ */
+public class Harjutused {
+    public static void main(String[] args) {
+        int [] array = new int[2];
+        array[0]=1;
+        array[1]=2;
+        array[2]=3;
+        System.out.println(array.toString());
+    }
+}
